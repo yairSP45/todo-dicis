1237683f1877df5a6f0332bc0c9349e1de16e856
yairSP45 <sanabria.porras.yair@gmail.com>
2023-08-28 11:03:47 -0600

    Terminamos todo

--- js/todo.js
+++ js/todo.js
@@ -14,6 +14,48 @@ document.addEventListener('DOMContentLoaded', () => {
     pintarTareas()
 })
 
+formulario.addEventListener('submit', e => {
+    e.preventDefault()
+    setTarea(e)
+})
+
+listaTareas.addEventListener('click', e => {
+    btnAcciones(e)
+})
+const btnAcciones = e => {
+    if(e.target.classList.contains('fa-circle-check')) {
+        tareas[e.target.dataset.id].estado = true
+        pintarTareas()
+    }
+    if(e.target.classList.contains('fa-undo-alt')) {
+        tareas[e.target.dataset.id].estado = false
+        pintarTareas()
+    }
+    if(e.target.classList.contains('fa-circle-minus')) {
+        delete tareas[e.target.dataset.id]
+        pintarTareas()
+    }
+}
+
+const setTarea = e => {
+    const texto = e.target.querySelector('input').value
+
+    if(texto.trim() === ''){
+        alert('Tarea  Vacia')
+        return
+    }
+    const tarea = {
+        id: Date.now(),
+        texto,
+        estado: false
+    }
+
+    tareas[tarea.id] = tarea
+    pintarTareas()
+    formulario.reset()
+    e.target.querySelector('input').focus()
+}
+
 const pintarTareas = () => {
     localStorage.setItem('tareas', JSON.stringify(tareas))
     if(Object.values(tareas).length === 0){
@@ -30,12 +72,12 @@ const pintarTareas = () => {
         const clone = template.cloneNode(true)
         clone.querySelector('p').textContent = tarea.texto
         if(tarea.estado) {
-            clone.querySelectorAll('.fas')[0].classList.replace('fa-circle-check', 'fa-circle-minus')
+            clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-circle-check', 'fa-undo-alt')
             clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
             clone.querySelector('p').style.textDecoration = 'line-through'
         }
-        clone.querySelectorAll('.fast')[0].dataset.id = tarea.id
-        clone.querySelectorAll('.fast')[1].dataset.id = tarea.id
+        clone.querySelectorAll('.fa-solid')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fa-solid')[1].dataset.id = tarea.id
         fragment.appendChild(clone)
     })
     listaTareas.appendChild(fragment)
